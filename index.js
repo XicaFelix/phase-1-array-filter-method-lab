093d71d162a212aa54953d3ca84470db7fefbbe6
Xica Felix <xicasfelix@gmail.com>
2021-12-20 22:15:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array, string){
+   let match =  array.filter(string => string);
+   if (typeof match === 'undefined'){
+       match = array.filter(string => string.toLowerCase());
+       if (typeof match === 'undefined'){
+           return [];
+       } 
+   }
+   return match;
+   
+}
+// function findMatching(array, string){
+ //   check if filtering for the string returns false:
+//   if the function returns false:
+ //       filter using string.toLowerCase 
+//           if filtering using string.toLowerCase returns false:
+//              return empty array
+//       if filtering for the string returns true:
+//            return the solution array
+//           
